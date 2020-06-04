6acaee686f4bf9931a2d436c9cbd1969e90a2575
Tung Pham <tung.pham@terralogic.com>
2020-06-04 18:14:09 +0700

    - Initial Commit

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '11.0'
+# Uncomment this line if you're using Swift
+use_frameworks!
+
+project 'Vanilla.xcodeproj'
+
+target 'Vanilla' do
+   pod 'RealmSwift'
+end
+
+
+post_install do |installer|
+    installer.pods_project.targets.each do |target|
+        target.build_configurations.each do |config|
+            config.build_settings['EXPANDED_CODE_SIGN_IDENTITY'] = ""
+            config.build_settings['CODE_SIGNING_REQUIRED'] = "NO"
+            config.build_settings['CODE_SIGNING_ALLOWED'] = "NO"
+        end
+    end
+end
+
---
+++ Vanilla/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Vanilla
+//
+//  Created by Tung Pham on 6/3/20.
+//  Copyright © 2020 Terralogic. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var vanillaMapView: MKMapView!
+    
+    fileprivate let locationManager: CLLocationManager = {
+        let manager = CLLocationManager()
+        manager.requestWhenInUseAuthorization()
+        return manager
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupMapView()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+    }
+    
+    //MARK: MapView Method
+    func setupMapView() {
+        self.vanillaMapView.showsUserLocation = true
+        self.vanillaMapView.showsCompass = true
+        self.vanillaMapView.showsScale = true
+        currentLocation()
+    }
+    
+    func currentLocation() {
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            if #available(iOS 11.0, *) {
+                locationManager.showsBackgroundLocationIndicator = true
+            }
+            else {
+                
+            }
+            
+            locationManager.startUpdatingLocation()
+        }
+    }
+
+}
+
+//MARK: - CLLocationManagerDelegate Methods
+extension ViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location = locations.last! as CLLocation
+        let currentLocation = location.coordinate
+        let coordinateRegion = MKCoordinateRegion(center: currentLocation, latitudinalMeters: 800, longitudinalMeters: 800)
+        self.vanillaMapView.setRegion(coordinateRegion, animated: true)
+        
+        //locationManager.stopUpdatingLocation()
+        
+        print("------------- %s", currentLocation)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("---------- error: %s", error.localizedDescription)
+    }
+}
+
